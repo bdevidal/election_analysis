45f3c57878381ee55e923614d4ef3791fb0c4170
Brendan deVidal <brendandevidal@Brendans-MacBook-Pro.local>
2020-10-18 18:32:21 -0500

    adding files

---
+++ temp_practice.py
@@ -0,0 +1,14 @@
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes:,} number of votes. "
+#     f"The total number of votes in the election was {total_votes:,}. "
+#     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+# print(message_to_candidate)
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for counties, voters in counties_dict
+    print(counties, voters)
+
